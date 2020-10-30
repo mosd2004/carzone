1d4a60b1301539d3d3ed62a1d51a76857987f8df
Mohamoud Dandan <mosd2004@hotmail.com>
2020-10-29 20:52:23 -0400

    configured database and whitenoise middleware

--- carzone/carzone/settings.py
+++ carzone/carzone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '_5u&r_ai^yd^*vutnj$y8l#)i)0(wj9%(2e)wnrcy6tid%b-5z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 LOGIN_REDIRECT_URL = 'dashboard'
@@ -61,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'carzone.urls'
@@ -87,16 +89,18 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'carzone_db',
-        'USER': 'postgres',
-        'PASSWORD': 'Dandan@77',
-        'HOST': 'localhost',
-        'PORT': '5433',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'carzone_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Dandan@77',
+#         'HOST': 'localhost',
+#         'PORT': '5433',
+#     }
+# }
+
+DATABASES = {'default': dj_database_url.config(default='postgres://postgres:Dandan@77@localhost/carzone_db')}
 
 
 # Password validation
@@ -164,3 +168,7 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mdandan2019@gmail.com'
 EMAIL_HOST_PASSWORD = 'Dandan@77'
 EMAIL_USE_TLS = True
+
+
+#whitenoise 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
